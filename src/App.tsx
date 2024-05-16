c06ef02d5cda50236204fdde86f0300e02025152
Ingrid Alves <alves96.ingrid@gmail.com>
2024-05-16 13:06:16 -0300

    Treinando React com Props e Hooks

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import './App.css'
+import Contador from './pages/contador/Contador'
+import Home from './pages/home/Home'
+import Tarefa from './pages/tarefa/Tarefa'
+
+function App() {
+
+  return (
+    <>
+        <Home 
+          titulo='Componete Homr'
+          texto='Este é um Componente que recebe props.'
+        />
+
+        <Contador/>
+
+        <Tarefa/>
+
+    </>
+  )
+}
+
+export default App
